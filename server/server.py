43529c4225edb5e47926b4c346162096e2686615
QiHaoBao <qihao.bao@sv.cmu.edu>
2017-12-21 18:05:27 -0800

    winter2017

--- server/server.py
+++ server/server.py
@@ -17,7 +17,7 @@ import networkx as nx
 
 from networkx.readwrite import json_graph
 
-JPL_HOST_GET_WORKFLOW_BY_ID = "http://localhost:9005/serviceExecutionLog/getServiceExecutionLogByWorkflowId/"
+JPL_HOST_GET_WORKFLOW_BY_ID = "http://hawking.sv.cmu.edu:9038/serviceExecutionLog/getServiceExecutionLogByWorkflowId/"
 PROM_DIR = "/home/soc/Downloads/prom/prom-6.6-all-platforms/"
 #JPL_HOST_GET_WORKFLOW_BY_ID = "http://hawking.sv.cmu.edu:9005/serviceExecutionLog/getServiceExecutionLogByWorkflowId/"
 #PROM_DIR = "/home/new/m66m/"
@@ -33,7 +33,7 @@ def test():
     print "Got Traces..."
     result = json.loads(result)
     print type(result)
-    
+
     traces = {}
     log = xes.Log()
     workflowId = result[0]['workflowId']
@@ -45,7 +45,7 @@ def test():
         else:
             traces["WRID" + str(x['workflowRunId'])].append(x['climateService']['serviceEntry']['name'])
         print "Name " + x['climateService']['serviceEntry']['name']
-        
+
     for y in traces:
         t = xes.Trace()
         for z in range(len(traces[y])):
@@ -56,30 +56,30 @@ def test():
             ]
             t.add_event(e)
         log.add_trace(t)
-        
+
     log.classifiers = [
         xes.Classifier(name="org:resource",keys="org:resource"),
         xes.Classifier(name="concept:name",keys="concept:name")
     ]
     open(PROM_DIR + "server.xes", "w").write(str(log))
-    print "=================================="   
-    
+    print "=================================="
+
     with open(PROM_DIR + "script_inductive_miner.txt", "rt") as fin:
         with open(PROM_DIR + "script_inductive_miner_workflow_" + workflowId + ".txt", "wt") as fout:
             for line in fin:
                 fout.write(line.replace('mined_net_server.pnml', 'mined_net_workflow_' + workflowId + '.pnml'))
-    
-    os.chdir(PROM_DIR)   
+
+    os.chdir(PROM_DIR)
     print os.system("pwd;sh " + PROM_DIR + "ProM66_CLI.sh -f " + PROM_DIR + "script_inductive_miner_workflow_" + workflowId + ".txt")
 
     with open(PROM_DIR + 'mined_net_workflow_' + workflowId + '.pnml', "rt") as fin:
         for i,line in enumerate(fin):
             if i == 1:
                 jsoned = bf.data(fromstring(line))
-    
+
     with open(PROM_DIR + "jsoned_workflow_" + workflowId + ".txt", "wt") as fout:
         fout.write(json.dumps(jsoned))
-        
+
     G=nx.DiGraph()
 
     page = jsoned['pnml']['net']['page']
@@ -95,7 +95,7 @@ def test():
     for a in page['arc']:
         #print a['@id']
         G.add_edge(a['@source'], a['@target'], name = a['name']['text']['$'], sort = 'arc')
-        
+
     test_res = set(n for u,v,d in G.edges_iter(data=True)
                if d['sort']=='arc'
                for n in (u, v)
@@ -112,20 +112,20 @@ def test():
                 uow.append((i[0], i[1]))
                 break
     print uow
-        
+
     jsonRes = {"all_uows" : []}
     for pair in uow:
         sub = nx.DiGraph()
-        for path in nx.all_simple_paths(G, source=pair[0], target=pair[1]):         
+        for path in nx.all_simple_paths(G, source=pair[0], target=pair[1]):
             for node in path:
                 sub.add_node(node)
         subG = nx.subgraph(G, sub)
         print subG.nodes()
         #nx.draw_networkx(subG)
-        #plt.show()    
+        #plt.show()
         jsonTmp = json_graph.node_link_data(subG)
         jsonRes['all_uows'].append(jsonTmp)
-    
+
     return json.dumps(jsonRes)
 
 @app.route("/ttt", methods=["GET"])
@@ -138,4 +138,3 @@ def ttt():
 if __name__ == "__main__":
     app.run(port=5000, host= '0.0.0.0', debug=True)
     #app.run(port=5000, host= '127.0.0.1', debug=True)
-
